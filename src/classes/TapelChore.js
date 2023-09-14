9861d4bb66a4ae224233d8856958ec254f18a28f
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-14 15:02:21 -0500

    Update TapelChore.js

--- src/classes/TapelChore.js
+++ src/classes/TapelChore.js
@@ -1,1 +1,23 @@
+module.exports = 
 
+    
+(c) => ({
+        
+        data: class TapelChore {
+		    constructor(/**/) {
+		        let args = Array.from(arguments);
+		        let name = args.shift();
+		        let string = args.pop();
+		        
+		        let stuff =  new Function( ...args, string )
+		        
+		        Object.defineProperty( c.prototype, name, { value: stuff });
+		
+		        return stuff;
+		    }
+		},
+
+        
+        refs: [ "Chore", "chore" ]
+        
+});
