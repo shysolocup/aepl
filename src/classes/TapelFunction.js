0bc985728c02dcc8c0aaae3201d19af0c14274d2
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-14 14:22:36 -0500

    Update TapelFunction.js

--- src/classes/TapelFunction.js
+++ src/classes/TapelFunction.js
@@ -1,1 +1,20 @@
 
+module.exports = 
+    
+c => {
+        
+        data: class TapelFunction {
+            constructor(name, func) {
+                let stuff = (func instanceof Function) ? func : function() { return func; }
+        
+                Object.defineProperty(stuff, "name", { value: name });
+                Object.defineProperty( c.prototype, name, { value: stuff });
+        
+                return stuff;
+            }
+        },
+
+        
+        refs: [ "Function", "function", "Func", "func"]
+        
+}
