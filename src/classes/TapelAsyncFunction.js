9dc43cb1b2fec1f71aa14d505c42f86f1c438b06
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-14 14:37:03 -0500

    Update TapelAsyncFunction.js

--- src/classes/TapelAsyncFunction.js
+++ src/classes/TapelAsyncFunction.js
@@ -1,1 +1,20 @@
+module.exports = 
 
+    
+(c) => ({
+        
+        data: class TapelAsyncFunction {
+            constructor(name, func) {
+                let stuff = (func instanceof Function) ? func : async function() { return func; }
+
+                Object.defineProperty(stuff, "name", { value: name });
+                Object.defineProperty( c.prototype, name, { value: stuff });
+
+                return stuff;
+            }
+        },
+
+        
+        refs: [ "AsyncFunction", "asyncFunction", "AsyncFunc", "asyncFunc" ]
+        
+})
