d102200eb389e8fa3f8a0d94241b8a6aed33cfc6
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-14 14:10:59 -0500

    Update ClassBuilder.js

--- src/builder/ClassBuilder.js
+++ src/builder/ClassBuilder.js
@@ -1,1 +1,20 @@
+const { Soup } = require('stews');
+const fs = require('fs');
 
+
+function ClassBuilder(c) {
+    let files = fs.readdirSync(`src/makers`).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) );
+    files.forEach( (file) => {
+        let { refs, data} = require(`../makers/${file}`);
+
+        refs.forEach( (ref) => {
+            Object.defineProperty(c, ref, data);
+        })
+    });
+
+    
+	return c;
+}
+
+
+module.exports = ClassBuilder;
