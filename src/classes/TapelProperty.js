0ac272aaf407e319171627f5c46b1a3c01f6a91f
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-14 14:59:35 -0500

    Update TapelProperty.js

--- src/classes/TapelProperty.js
+++ src/classes/TapelProperty.js
@@ -1,1 +1,25 @@
+module.exports = 
 
+    
+(c) => ({
+        
+        data: class TapelProperty {
+		    constructor(name, value, attributes={set:undefined, enumerable:false, configurable:false}) {
+		        let func = (value instanceof Function) ? value : function() { return value; };
+		        
+		        Object.defineProperty(func, "name", { value: name });
+		        Object.defineProperty(c.prototype, name, {
+		            get: func,
+		            set: attributes.set,
+		            enumerable: attributes.enumerable,
+		            configurable: attributes.configurable
+		        });
+		
+		        return func;
+		    }
+		},
+
+        
+        refs: [ "Property", "property", "Prop", "prop" ]
+        
+});
