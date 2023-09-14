e7e5ca17a184e4056bac9566ace1d5c5ac32beb8
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-14 15:04:12 -0500

    Update TapelAsyncChore.js

--- src/classes/TapelAsyncChore.js
+++ src/classes/TapelAsyncChore.js
@@ -1,1 +1,25 @@
+const AsyncFunction = Object.getPrototypeOf(async function(){}).constructor;
 
+module.exports = 
+
+    
+(c) => ({
+        
+        data: class TapelAsyncChore {
+		    constructor(/**/) {
+		        let args = Array.from(arguments);
+		        let name = args.shift();
+		        let string = args.pop();
+		        
+		        let stuff =  new AsyncFunction( ...args, string )
+		        
+		        Object.defineProperty( c.prototype, name, { value: stuff });
+		
+		        return stuff;
+		    }
+		},
+
+        
+        refs: [ "AsyncChore", "asyncChore" ]
+        
+});
