7fcbc618803242ec9b27ac9e11640855cb0c98c9
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-14 15:07:41 -0500

    Update TapelClass.js

--- src/classes/TapelClass.js
+++ src/classes/TapelClass.js
@@ -1,1 +1,30 @@
+const builder = require('../builder');
 
+
+module.exports = 
+
+
+(c) => ({
+        
+        data: class TapelClass {
+		    constructor(name, value) {
+		        let cl = builder(
+			    	(value instanceof Function) ? value : class { constructor() { return value; } } 
+			    );
+		
+		        Object.defineProperty(cl, "name", { value: name });
+		        Object.defineProperty(c.prototype, name, { 
+					get() {
+						cl.prototype.parent = this;
+						return cl;
+					}
+				});
+					
+				return cl;
+			}
+		},
+
+        
+        refs: [ "Class", "class" ]
+        
+});
