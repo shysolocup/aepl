d2a2a9ad858f7900b071d1b77801e2fa2be09dc3
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-18 13:31:56 -0500

    Update index.js

--- src/builder/index.js
+++ src/builder/index.js
@@ -1,20 +1,40 @@
 const fs = require('fs');
+let exports = {};
+
 
 
 function ClassBuilder(c) {
-    let files = fs.readdirSync(`node_modules/aepl/src/classes`).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+    let classes = fs.readdirSync(`node_modules/aepl/src/classes`).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+	let functions = fs.readdirSync(`node_modules/aepl/src/functions`).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+
 	
-    files.forEach( (file) => {
-        let { refs, data } = require(`../classes/${file}`)(c);
+	// adds classes
+    classes.forEach( (file) => {
+        let { refs, data, name } = require(`../classes/${file}`)(c);
+
+		exports[name] = data;
 
         refs.forEach( (ref) => {
             Object.defineProperty(c, ref, { value: data });
-        })
+        });
     });
 
-    
+	
+	// adds functions
+	functions.forEach( (file) => {
+        let { refs, data } = require(`../functions/${file}`)(c);
+
+        refs.forEach( (ref) => {
+            Object.defineProperty(c, ref, { value: data });
+        });
+    });
+
+	
 	return c;
+
 }
 
 
-module.exports = ClassBuilder;
+
+exports.push(ClassBuilder);
+module.exports = exports;
