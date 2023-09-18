f3b40b10510c92102230139cc7f9a74be8d0a537
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-18 13:15:37 -0500

    Update addFunction.js

--- src/functions/addFunction.js
+++ src/functions/addFunction.js
@@ -1,1 +1,19 @@
+module.exports = 
 
+    
+(c) => ({
+        
+        data: function AeplAddFunction(name, func) {
+                let stuff = (func instanceof Function) ? func : function() { return func; }
+        
+                Object.defineProperty(stuff, "name", { value: name });
+                Object.defineProperty( c.prototype, name, { value: stuff });
+        
+                return stuff;
+            }
+        },
+
+        
+        refs: [ "addFunction", "addFunc" ]
+        
+});
