3bdb60efd9ec37d8af53726c5cb0d7fbe02c2857
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-18 13:47:24 -0500

    Update AeplAddFunction.js

--- src/functions/AeplAddFunction.js
+++ src/functions/AeplAddFunction.js
@@ -4,9 +4,8 @@ module.exports =
 (c) => ({
         
         data: function AeplAddFunction(/**/) {
-                const { data } = require('../classes/AeplFunction.js')(c);
-                return new data(...Array.from(arguments));
-            }
+            const { data } = require('../classes/AeplFunction.js')(c);
+            return new data(...Array.from(arguments));
         },
 
         
