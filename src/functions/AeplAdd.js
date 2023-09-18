6cfec95151856570b07814a358c334084fa0378d
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-18 14:01:12 -0500

    Create AeplAdd.js

---
+++ src/functions/AeplAdd.js
@@ -0,0 +1,28 @@
+module.exports = 
+
+    
+(c) => ({
+        
+        data: function AeplAdd(/**/) {
+            let classes = fs.readdirSync(`node_modules/aepl/src/classes`).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
+            
+            let args = Array.from(arguments);
+            let type = args.shift();
+
+            var thing;
+
+            classes.forEach( (file) => {
+                let { refs, data } = require(`../classes/${file}`)(c);
+
+                refs.forEach( (ref) => {
+                    if (ref == type) thing = data;
+                });
+            });
+
+            return new thing(...args);
+        },
+
+        
+        refs: [ "add", "create" ]
+        
+});
