a884edc1ffbdb7c1e45bd825615ec1a53af1803a
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-18 13:48:15 -0500

    Update AeplAddSubClass.js

--- src/functions/AeplAddSubClass.js
+++ src/functions/AeplAddSubClass.js
@@ -4,9 +4,8 @@ module.exports =
 (c) => ({
         
         data: function AeplAddSubClass(/**/) {
-                const { data } = require('../classes/AeplSubClass.js')(c);
-                return new data(...Array.from(arguments));
-            }
+            const { data } = require('../classes/AeplSubClass.js')(c);
+            return new data(...Array.from(arguments));
         },
 
         
