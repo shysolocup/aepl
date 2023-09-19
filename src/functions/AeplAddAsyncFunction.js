00d6d5a5bf9dbd83596f7a23a522115991c9308a
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-19 13:09:50 -0500

    Update AeplAddAsyncFunction.js

--- src/functions/AeplAddAsyncFunction.js
+++ src/functions/AeplAddAsyncFunction.js
@@ -9,6 +9,6 @@ module.exports =
         },
 
         
-        refs: [ "addAsyncFunction", "addAsyncFunc" ]
+        refs: [ "addAsyncFunction", "addAsyncFunc", "addAFunction", "addAFunc" ]
         
 });
