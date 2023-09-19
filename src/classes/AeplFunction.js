68fe830c59e397749d96ff4fa37e4b8708b1d280
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-19 14:46:40 -0500

    Update AeplFunction.js

--- src/classes/AeplFunction.js
+++ src/classes/AeplFunction.js
@@ -8,7 +8,9 @@ module.exports =
                 let stuff = (func instanceof Function) ? func : function() { return func; }
         
                 Object.defineProperty(stuff, "name", { value: name });
-                c.prototype[name] = stuff;
+                Object.defineProperty(c.prototype, name, {
+                    value: stuff
+                });
         
                 return stuff;
             }
