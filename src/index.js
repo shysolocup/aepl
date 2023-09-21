288d31bbb83113c809933e72fba3a556ec26486b
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-21 09:55:12 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ const { builder } = require('./builder');
 
 class Class {
     constructor(name, c, autodefine=true) {
+        if (c == undefined) c = class {};
+
+
         Object.defineProperty(c, "name", { value: name });
     	c = builder(c);
     	
+        
        	if (autodefine) return new Function("c", `return ${name} = c`)(c);
         else return c;
     }
 }
+
 Class = builder(Class);
 
 
