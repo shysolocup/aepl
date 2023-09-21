fd0ffd1ded841c0b48482e2838af5abbc0024df6
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-21 13:00:06 -0500

    Update index.js

--- src/builder/index.js
+++ src/builder/index.js
@@ -5,8 +5,8 @@ let exp = {};
 function ClassBuilder(c) {
 	if (c == undefined) c = class {};
 	
-	let classDir = require('../classes/dir');
-	let funcDir = require('../functions/dir');
+	let classDir = require('../classes/class_dir').replace("class_dir", "");
+	let funcDir = require('../functions/func_dir').replace("func_dir", "");
 	
     let classes = fs.readdirSync(classDir).filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
 	let functions = fs.readdirSync().filter( file => ((file.endsWith('.js') || file.endsWith('.ts')) ));
