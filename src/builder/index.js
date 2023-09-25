87c3aba9dbd280fd8521657a9d6561f643fec938
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-25 13:13:20 -0500

    Updated builder

--- src/builder/index.js
+++ src/builder/index.js
@@ -4,6 +4,26 @@ let exp = {};
 
 function ClassBuilder(c) {
 	if (c == undefined) c = class {};
+    if (!(c instanceof Function)) {
+        let val = c;
+
+        c = class {
+            constructor() {
+                return new Proxy(val, {
+
+                    get(target, prop) {
+                        if (Object.getOwnPropertyNames(c.prototype).includes(prop)) {
+                            return c.prototype[prop];
+                        }
+                        else {
+                            return target[prop];
+                        }
+                    }
+
+                });
+            }
+        }
+    }
 	
 	let class_dir = require('../classes/funky_dir');
 	let func_dir = require('../functions/funky_dir');
