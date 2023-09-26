813facfb3628a9068ac27c3fc77a633753e09851
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-26 12:57:05 -0500

    scrapped the specifics for building classes with non-class values because I'm too lazy and too stupid to get it to work corrrectly

--- src/builder/index.js
+++ src/builder/index.js
@@ -4,31 +4,6 @@ let exp = {};
 
 function ClassBuilder(c) {
 	if (c == undefined) c = class {};
-    if (!(c instanceof Function)) {
-        let val = c;
-
-        c = class {
-            constructor() {
-                return new Proxy(val, {
-
-                    get(target, prop) {
-                        if (prop == "parent") { // if user is getting the parent
-                        	if (c.prototype.parent) return new c.prototype.parent.constructor();
-							
-							else return undefined;
-                        }
-                        else if (Object.getOwnPropertyNames(c.prototype).includes(prop)) { // if user is getting an aepl thing
-                            return c.prototype[prop];
-                        }
-                        else { // other
-                            return target[prop];
-                        }
-                    }
-
-                });
-            }
-        }
-    }
 	
 	let class_dir = require('../classes/funky_dir');
 	let func_dir = require('../functions/funky_dir');
