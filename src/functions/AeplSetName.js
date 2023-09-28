373980f4b0170914710a786ef625136f3a1e95cc
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-28 10:02:47 -0500

    Create AeplSetName.js

---
+++ src/functions/AeplSetName.js
@@ -0,0 +1,11 @@
+module.exports = 
+
+    
+(c) => ({
+        data: function AeplSetName(name) {
+            Object.defineProperty(c, "name", { value: name });
+        },
+        
+        refs: [ "setName", "setN" ]
+        
+});
