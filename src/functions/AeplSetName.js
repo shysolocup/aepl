05813735d6305e8a594320ceb735350b2de16a0c
paige <88659700+paigeroid@users.noreply.github.com>
2023-09-28 10:15:08 -0500

    Update AeplSetName.js

--- src/functions/AeplSetName.js
+++ src/functions/AeplSetName.js
@@ -4,7 +4,7 @@ module.exports =
     
 (c) => ({
     
-        data: function AeplSetName(name, autodefine) {
+        data: function AeplSetName(name, autodefine=true) {
             Object.defineProperty(c, "name", { value: name });
             if (autodefine) return new Function("cl", `return ${name} = cl`)(c);
         },
