95ac31ad295016474eee19bb35a9baa71cb1ad8b
paige <88659700+paishee@users.noreply.github.com>
2023-11-11 21:47:09 -0600

    Create AeplPreFunction.js

---
+++ src/classes/AeplPreFunction.js
@@ -0,0 +1,22 @@
+module.exports = 
+
+    
+(c) => ({
+        
+        data: class AeplPreFunction {
+            constructor(name, func) {
+                let stuff = (func instanceof Function) ? func : function() { return func; }
+        
+                Object.defineProperty(stuff, "name", { value: name });
+                Object.defineProperty(c, name, {
+                    value: stuff
+                });
+        
+                return stuff;
+            }
+        },
+
+        
+        refs: [ "PreFunction", "preFunction", "PreFunc", "preFunc" ]
+        
+});
