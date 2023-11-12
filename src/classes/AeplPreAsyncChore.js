1a1bfbbdaa14372ee2ab280e51757e3588a0ed7e
paige <88659700+paishee@users.noreply.github.com>
2023-11-11 21:54:43 -0600

    Create AeplPreAsyncChore.js

---
+++ src/classes/AeplPreAsyncChore.js
@@ -0,0 +1,27 @@
+const AsyncFunction = Object.getPrototypeOf(async function(){}).constructor;
+
+module.exports = 
+
+    
+(c) => ({
+        
+        data: class AeplPreAsyncChore {
+		    constructor(/**/) {
+		        let args = Array.from(arguments);
+		        let name = args.shift();
+		        let string = args.pop();
+		        
+		        let stuff =  new AsyncFunction( ...args, string )
+		        
+		        Object.defineProperty(c, name, {
+	                    value: stuff
+	                });
+		
+		        return stuff;
+		    }
+		},
+
+        
+        refs: [ "PreAsyncChore", "preAsyncChore", "preAChore", "PreAChore" ],
+        
+});
