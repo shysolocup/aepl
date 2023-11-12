aa63d9aeb1ab9f8bee435c84d834b954ee83fc16
paige <88659700+paishee@users.noreply.github.com>
2023-11-11 21:47:54 -0600

    Create AeplPreProperty.js

---
+++ src/classes/AeplPreProperty.js
@@ -0,0 +1,25 @@
+module.exports = 
+
+    
+(c) => ({
+        
+        data: class AeplPreProperty {
+		    constructor(name, value, attributes={set:undefined, enumerable:false, configurable:false}) {
+		        let func = (value instanceof Function) ? value : function() { return value; };
+		        
+		        Object.defineProperty(func, "name", { value: name });
+		        Object.defineProperty(c, name, {
+		            get: func,
+		            set: attributes.set,
+		            enumerable: (attributes.enumerable) ? attributes.enumerable : true,
+		            configurable: (attributes.configurable) ? attributes.configurable : true
+		        });
+		
+		        return func;
+		    }
+		},
+
+        
+        refs: [ "PreProperty", "preProperty", "PreProp", "preProp" ]
+        
+});
