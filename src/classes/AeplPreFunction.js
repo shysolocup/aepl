4e334c3bd4bfb66cef21221778333c2732f24336
paige <88659700+paishee@users.noreply.github.com>
2024-01-31 11:04:50 -0600

    Update AeplPreFunction.js

--- src/classes/AeplPreFunction.js
+++ src/classes/AeplPreFunction.js
@@ -8,6 +8,7 @@ module.exports =
                 let stuff = (func instanceof Function) ? func : function() { return func; }
         
                 Object.defineProperty(stuff, "name", { value: name });
+				Object.defineProperty(stuff, "parent", { value: c });
                 Object.defineProperty(c, name, {
                     value: stuff
                 });
