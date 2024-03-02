ae1a0b876d2894b6a7e2ae7a12cbd0e35c7c56a5
paige <88659700+paishee@users.noreply.github.com>
2024-03-02 03:10:38 -0600

    Update AeplFunction.js

--- src/classes/AeplFunction.js
+++ src/classes/AeplFunction.js
@@ -7,8 +7,8 @@ module.exports =
             constructor(name, func) {
                 let stuff = (func instanceof Function) ? func : function() { return func; }
         
-                Object.defineProperty(stuff, "name", { value: name });
-				Object.defineProperty(stuff, "parent", { value: c });
+                Object.defineProperty(stuff, "name", { value: name, configurable: true });
+				Object.defineProperty(stuff, "parent", { value: c, configurable: true });
                 Object.defineProperty(c.prototype, name, {
                     value: stuff
                 });
